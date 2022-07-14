a770c14d9cce653074dbc92c41fdc5ff2f7e5bae
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-07-14 13:51:42 +0900

    update

--- 2022-07/othello.java
+++ 2022-07/othello.java
@@ -95,6 +95,48 @@ public class othello {
         return count;
     }
 
+    static int[][] board_copy(int board[][]) {
+        int[][] copy = new int[board.length][board.length];
+        for (int i = 0; i < board.length; i++) {
+            for (int ii = 0; ii < board[i].length; ii++) {
+                copy[i][ii] = board[i][ii];
+            }
+        }
+        return copy;
+    }
+
+    static int get_score(int board[][], int y, int x, int player, int recursive) {
+        int point = 0;
+        if (board[y][x] != 0)
+            return 0;
+        if (reverse(board, y, x, player, true) == 0)
+            return 0;
+        if (y == 0 || y == board.length - 1) {
+            point += 3;
+        }
+        if (x == 0 || x == board.length - 1) {
+            point += 3;
+        }
+
+        int[][] copy = board_copy(board);
+        point += reverse(copy, y, x, player, false);
+
+        if (recursive > 0) {
+            int max_score = 0;
+            for (int i = 0; i < board.length; i++) {
+                for (int ii = 0; ii < board[i].length; ii++) {
+                    int score = get_score(copy, i, ii, player % 2 + 1, recursive - 1);
+                    if (score > max_score) {
+                        max_score = score;
+                    }
+                }
+            }
+            point -= max_score;
+
+        }
+        return point;
+    }
+
     public static void main(String[] args) {
         int[][] board = new int[8][8];
         Scanner sc = new Scanner(System.in);
@@ -104,8 +146,10 @@ public class othello {
         System.out.println("0 2人で遊ぶ");
         System.out.println("1 Botと遊ぶ(先行)");
         System.out.println("2 Botと遊ぶ(後攻)");
+        System.out.println("3 Botと遊ぶ 高難易度 高負荷(先行)");
+        System.out.println("4 Botと遊ぶ 高難易度 高負荷(後攻)");
         int mode = sc.nextInt();
-        if (mode == 2)
+        if (mode == 2 || mode == 4)
             player = 2;
 
         init(board);
@@ -148,24 +192,17 @@ public class othello {
                 }
 
             } else {
-                int max_point = 0;
+                System.out.println("計算中です");
+                int max_score = 0;
+                int recursive = 4;
+                if (mode == 3 || mode == 4) {
+                    recursive = 6;
+                }
                 for (int i = 0; i < board.length; i++) {
                     for (int ii = 0; ii < board[i].length; ii++) {
-                        int point = 0;
-                        if (board[i][ii] != 0)
-                            continue;
-                        if (reverse(board, i, ii, player, true) == 0)
-                            continue;
-                        if (i == 0 || i == board.length - 1) {
-                            point += 3;
-                        }
-                        if (ii == 0 || ii == board.length - 1) {
-                            point += 3;
-                        }
-                        point += reverse(board, i, ii, player, true);
-
-                        if (point > max_point) {
-                            max_point = point;
+                        int score = get_score(board, i, ii, player, recursive);
+                        if (score > max_score) {
+                            max_score = score;
                             y = i;
                             x = ii;
                         }
