dce1f1243f9f7eccafd0bb894af3b6935afd44c8
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-10-25 10:12:20 +0900

    add 20221025

--- 2022-10/MoneyCount.java
+++ 2022-10/MoneyCount.java
@@ -1,11 +1,18 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class MoneyCount {
     public static void main(String[] args) {
         System.out.print("金額を入力してください→ ");
         Scanner sin = new Scanner(System.in);
-        int price = sin.nextInt();
+        int price = 0;
+        try {
+            price = sin.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("数字を入力してください");
+            System.exit(1);
+        }
         int coin;
         sin.close();
 
