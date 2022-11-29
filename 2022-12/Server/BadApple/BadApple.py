53c79afecc46a1cad7aa4e77b3a09c3d98bc4f80
ふぁ <yuki@yuki0311.com>
2022-11-29 14:07:34 +0900

    add 20221129
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- 2022-12/Server/BadApple/BadApple.py
+++ 2022-12/Server/BadApple/BadApple.py
@@ -9,6 +9,7 @@ s.bind(("0.0.0.0", 49152))
 s.listen(5)
 
 files = glob.glob("assets/badapple/*.jpg")
+scale = 8
 
 while True:
     print("Waiting...")
@@ -20,17 +21,28 @@ while True:
             height = img.shape[0]
             width = img.shape[1]
             output = ""
-            for y in range(0, height, 8):
-                for x in range(0, width, 8):
+            for y in range(0, height, scale * 2):
+                output += "\n"
+            for y in range(0, height, scale * 2):
+                for x in range(0, width, scale):
                     imgBox = img[y, x]
-                    if np.all(imgBox == np.array([255,255,255])):
+                    if height > y + scale:
+                        upImgBox = img[y + scale, x]
+                    else:
+                        upImgBox = imgBox
+
+                    if np.all(imgBox == np.array([255,255,255])) and np.all(upImgBox == np.array([255,255,255])):
                         output += ":"
+                    elif np.all(imgBox == np.array([255,255,255])):
+                        output += "'"
+                    elif np.all(upImgBox == np.array([255,255,255])):
+                        output += "."
                     else:
                         output += " "
                 output += "\n"
 
             clientsocket.send(output.encode())
             time.sleep(1/20)
-    except:
-        pass
+    except Exception as e:
+        print(e)
     clientsocket.close()
