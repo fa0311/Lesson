0469f45f519349ba5fe2edfd6c3d971c6e114208
ふぁ <yuki@yuki0311.com>
2022-12-07 15:13:58 +0900

    update
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- 2022-12/DB/src/DBManager.java
+++ 2022-12/DB/src/DBManager.java
@@ -1,12 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class DBManager {
     static final String JDBC_DRIVER = "org.h2.Driver";
-    //static final String DB_URL = "jdbc:h2:~/test";
-    //static final String DB_URL = "jdbc:h2:file:C:/data/example";   //組み込みモード
-    static final String DB_URL = "jdbc:hsqldb:hsql://localhost/";      // サーバーモード
+    // static final String DB_URL = "jdbc:h2:~/test";
+    // static final String DB_URL = "jdbc:h2:file:C:/data/example"; //組み込みモード
+    static final String DB_URL = "jdbc:hsqldb:hsql://localhost/"; // サーバーモード
 
     // Database credentials
     static final String USER = "sa";
@@ -14,30 +13,29 @@ public class DBManager {
 
     public static Connection getConnection() {
         Connection con = null;
-        try{
+        try {
             // hsqldbのJDBCドライバを指定する
             // (クラス名を指定してクラスをロードする。利用するJDBCの種類によって異なる)
             Class.forName(JDBC_DRIVER);
             // データベースに接続する
             con = DriverManager.getConnection(
-                             DB_URL,	// URL
-                             USER,		// ユーザー名
-                             PASS);		// パスワード
+                    DB_URL, // URL
+                    USER, // ユーザー名
+                    PASS); // パスワード
             // データベースコネクションオブジェクトを返す
             return con;
-        } catch(Exception e) {
-            //Handle errors for Class.forName
+        } catch (Exception e) {
+            // Handle errors for Class.forName
             e.printStackTrace();
         }
         return con;
     }
 }
 
-class DBManagerTest{  
+class DBManagerTest {
     public static void main(String[] args) throws Exception {
         // データベースと接続する
-        DBManager db = new DBManager();
-        Connection con = db.getConnection();
+        Connection con = DBManager.getConnection();
         // データベースのコネクション内容を表示
         System.out.println("con = " + con);
         // データベースを切断する(必ず行う)
--- 2022-12/DB/src/DeleteSample.java
+++ 2022-12/DB/src/DeleteSample.java
@@ -1,27 +1,17 @@
 import java.sql.Connection;
-import java.sql.Statement;
-
 
 public class DeleteSample {
    public static void main(String[] args) {
       try {
-         
+
          // データベースと接続する
          Connection con = DBManager.getConnection();
-            
 
-         
+         con.close(); // データベース切断する
 
-
-
-
-
-
-         con.close();    // データベース切断する
-         
       } catch (Exception e) {
          e.printStackTrace();
-      }   
+      }
 
-   } 
+   }
 }
